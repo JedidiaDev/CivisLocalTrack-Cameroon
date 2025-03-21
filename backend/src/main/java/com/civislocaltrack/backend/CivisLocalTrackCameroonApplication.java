3395a6cddbaf950804d56a96e333d5191df51ba5
JedidiaDev <kamdemjedidia@gmail.com>
2025-03-21 18:09:27 +0100

    Test du backend

--- backend/src/main/java/com/civislocaltrack/backend/CivisLocalTrackCameroonApplication.java
+++ backend/src/main/java/com/civislocaltrack/backend/CivisLocalTrackCameroonApplication.java
@@ -2,6 +2,8 @@ package com.civislocaltrack.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
 public class CivisLocalTrackCameroonApplication {
@@ -9,5 +11,10 @@ public class CivisLocalTrackCameroonApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CivisLocalTrackCameroonApplication.class, args);
 	}
+	//Test du backend spring
+	@GetMapping("/hello")
+    public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+      return String.format("Hello %s!", name);
+    }
 
 }
