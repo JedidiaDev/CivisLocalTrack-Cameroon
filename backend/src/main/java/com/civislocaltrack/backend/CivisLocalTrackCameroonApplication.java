808be5c79b2e476de315e7dd4a6e93e3d7f4268a
JedidiaDev <kamdemjedidia@gmail.com>
2025-03-21 16:35:15 +0100

    Initial commit

---
+++ backend/src/main/java/com/civislocaltrack/backend/CivisLocalTrackCameroonApplication.java
@@ -0,0 +1,13 @@
+package com.civislocaltrack.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivisLocalTrackCameroonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CivisLocalTrackCameroonApplication.class, args);
+	}
+
+}
